79658ba5a49a911e27c38e1105ad3aa18f5b2c13
sergiomieic <sergio.mieic@gmail.com>
2015-04-23 13:17:08 +0100

    Lolll

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,8 @@ int main() {
 	vector<Contentor> res;
 	res = g.getfloydWarshallPath(Contentor(1, "inicio", 0, 0),
 			Contentor(5, "fim", 0, 0));
-	cout << "Tamanho:" << res.size() << endl;
-
-	//	cout<<res[0].getId()<<endl;
-//	cout<<res[1].getId()<<endl;
-//	cout<<res[2].getId()<<endl;
 
 	showGraph(g);
-
 	cin.get();
-
 	return 0;
 }
